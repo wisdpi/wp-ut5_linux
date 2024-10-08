c20767570ad730941b1a0cb34573eefeba6b68b4
WisdPi <admin@wisdpi.com>
2024-10-08 17:52:25 +0800

    update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -16,31 +16,8 @@ module=${module%.ko}
 
 echo "DEPMOD $(uname -r)"
 depmod `uname -r`
-echo "load module $module"
-modprobe $module
-
-is_update_initramfs=n
-distrib_list="ubuntu debian"
-
-if [ -r /etc/debian_version ]; then
-	is_update_initramfs=y
-elif [ -r /etc/lsb-release ]; then
-	for distrib in $distrib_list
-	do
-		/bin/grep -i "$distrib" /etc/lsb-release 2>&1 /dev/null && \
-			is_update_initramfs=y && break
-	done
-fi
-
-if [ "$is_update_initramfs" = "y" ]; then
-	if which update-initramfs >/dev/null ; then
-		echo "Updating initramfs. Please wait."
-		update-initramfs -u -k $(uname -r)
-	else
-		echo "update-initramfs: command not found"
-		exit 1
-	fi
-fi
+echo "UPDATE initramfs"
+update-initramfs -u
 
 echo "COPY udev rules"
 cp 50-wput5.rules /etc/udev/rules.d/
